6e8971bee586929502a29ee6d550f7d0b76be92e
Michael Lobman <michael.lobman@gmail.com>
2022-01-12 11:58:51 -0500

    Complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    def name=(value)
+        @this_dogs_name = value
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
